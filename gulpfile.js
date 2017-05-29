ffe0e79e99a7b7cf706466304bf38c286053f15d
Julien Klopfer <julien.klopfer@gmail.com>
2017-05-28 22:33:35 -0400

    Replace CSS by LESS, use GULP to build and optimize the project files

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var 
+	gulp = require('gulp'),
+  less = require('gulp-less'),
+  cssmin = require('gulp-cssmin'),
+  plumber = require('gulp-plumber'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+	htmlclean = require('gulp-htmlclean');
+
+gulp.task('styles', function () {
+	
+	gulp.src('assets/styles/styles.less')
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(gulp.dest('dist'))
+		.pipe(cssmin())	
+		.pipe(rename({
+		    suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist'));
+		
+	gulp.src('assets/styles/img/*')
+		.pipe(plumber())
+		.pipe(gulp.dest('dist'));
+		
+});
+
+gulp.task('scripts', function() {  
+    
+	gulp.src('assets/scripts/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+	  .pipe(rename({
+	      suffix: '.min'
+	  }))	
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function() {  
+    
+	gulp.src('assets/html/*.html')
+    .pipe(htmlclean())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+	
+  gulp.watch('assets/styles/*.less', ['styles']);
+		
+	gulp.watch('assets/scripts/*.js', ['scripts']);
+		
+	gulp.watch('assets/html/*.html', ['html']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'html', 'watch']);
